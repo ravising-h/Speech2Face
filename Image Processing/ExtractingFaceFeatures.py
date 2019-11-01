52861891211e730512852f2dc34355606816f390
RAVI SINGH <43233175+ravising-h@users.noreply.github.com>
2019-11-01 21:04:58 +0530

    corrected missing portion added CNN Face detector file

--- Image Processing/ExtractingFaceFeatures.py
+++ Image Processing/ExtractingFaceFeatures.py
@@ -17,6 +17,8 @@ from tqdm import tqdm
 # Face Detection
 #############################
 
+cnn_face_detector = dlib.cnn_face_detection_model_v1("mmod_human_face_detector.dat")
+
 def detect(gray, frame):
     """
     
